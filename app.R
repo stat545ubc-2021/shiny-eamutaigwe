61463631a0e47e3c5c7f83233ecc55a17ae34345
Ekpereka Amutaigwe <ekpereku@student.ubc.ca>
2021-11-27 14:33:37 -0800

    update app.R file

--- app.R
+++ app.R
@@ -18,7 +18,7 @@ ui <- fluidPage(
   # Application title
   titlePanel("Characteristics of Penguins"),
   
-  # Brief description on the features in the Appication
+  # Brief description of the features in the Appication
   
   h5("This app provides an avenue to visually explore the various size relationships 
   in species of penguins on 3 islands. The palmerpenguins::penguins dataset is used
@@ -30,101 +30,85 @@ ui <- fluidPage(
   (i.e. variables) to map to the x- and y-coordinates. 
   3. The user can also give the plot a custom title based on characteristics plotted.
   4. There is also an option to study the penguins within a specific year. 
-  5. The user can select one or more years of interest manually or by pressing the play button.
+  5. The user can select year of interest manually or by pressing the play button.
      The play can be paused at will."), 
     
-    # Sidebar with selection of country and other variables
+  br(),
+  
+  # Sidebar for selection of species and other variables
     sidebarLayout(
         sidebarPanel(
           h4(strong("Characteristic inputs")),
-          
+
           # Create drop-down menu for species selection
           selectInput("species",
                       label = "Select species:",
                       choices = c("Adelie", "Chinstrap", "Gentoo"),
-                      selected = "Adelie"),
-          
-          # Create groups using species
-          #checkboxGroupInput("speciesInput",
-                             #label = "Species:",
-                             #choices = c("Adelie", "Chinstrap", "Gentoo"),
-                             #selected = "Adelie",
-          #),
-          
-          # Select a variable from the data frame
+                      selected = "Adelie"
+          ),
+          # Select a variable for x-axis from the data frame
           varSelectInput("X_Axis",
                          "Select variable for the X_axis", 
                          data = Filter(is.numeric, penguins), 
                          selected = "body_mass_g"
           ),
+          # Select a variable for y-axis from the data frame
           varSelectInput("Y_Axis",
                           label = "Select variable for the Y-axis",
                           data = Filter(is.numeric, penguins),
                           selected = "bill_length_mm"
           ),
+          # Select a variable to map to size from the data frame
           varSelectInput("size",
                          label = "Select variable to map to size of points",
                          data = Filter(is.numeric, penguins),
                          selected = "bill_depth_mm"
-        ),
-        selectInput("colour",
+          ),
+          # Select a variable to map to colour from the data frame
+          selectInput("colour",
                        label = "Select variable to map to colour of points",
                        choices = c("sex", "island"),
                        selected = "sex"
-        ),
-          
-        textInput("plot_name", 
-                  label = "Enter plot title", 
-                  placeholder = "Weight of penguin vs Bill length"
-        ),
-
-          # Create a slider input based on flipper length
-          #sliderInput("flipperInput",
-                        #"Flipper length of Interest:",
-                        #min = 172,
-                        #max = 231,
-                        #value = c(180),
-                        #animate = TRUE)
-        sliderInput("year",
-                    label = "Year of Interest:",
-                    min = 2007,
-                    max = 2009,
-                    value = 2007,
-                    animate = TRUE)
-        ),
-       
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("demoPlot"),
-            br(), br(),
-            #tableOutput("demoTable")
+          ),
+          # Create a placeholder for adding plot title
+          textInput("plot_name", 
+                    label = "Enter plot title", 
+                    placeholder = "Weight of penguin vs Bill length"
+          ),
+          # Create a slider input for year selection
+          sliderInput("year",
+                      label = "Year of Interest:",
+                      min = 2007,
+                      max = 2009,
+                      value = 2007,
+                      animate = TRUE)
+          ),
+          # Show a plot of the generated distribution
+          mainPanel(
+                    plotOutput("demoPlot"),
+                
+          br(), br(),
         )
    )
 )
 
-# Define server logic required to draw a bubble plot
+# Define server logic required to draw a scatter plot
 server <- function(input, output) {
     output$demoPlot <- renderPlot({
       penguins %>%
         drop_na() %>% 
-        arrange(flipper_length_mm) %>% 
           filter(species == input$species,
-                #flipper_length_mm >= input$flipperInput,
                 year >= input$year) %>% 
-                #species == input$speciesInput) %>% 
-      # Produce a bubble plot
+        
+      # Produce a scatter plot
       ggplot(aes_string(input$X_Axis, 
                         input$Y_Axis, 
                         size = input$size, 
                         colour = input$colour)) +
         geom_point(alpha = 0.7)  +
         ggtitle(input$plot_name) +
-        #theme(plot.title = element_text(face="bold",  size = 18)) +
         theme_bw()
     })
-    
-    #output$demoTable <- gapminder %>% 
-      #filter(year >= input$my_yearInput)
 }
 
 # Run the application 
